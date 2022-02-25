ea54a338db4e2991141106d47809bd01dd653011
Tehalexpetrov <tehalexpetrov@gmail.com>
2022-02-25 14:48:30 +0300

    Добавили нажатия на пункты меню

--- app/src/main/java/ru/tehalexpetrov/fishhandbook/MainActivity.kt
+++ app/src/main/java/ru/tehalexpetrov/fishhandbook/MainActivity.kt
@@ -2,10 +2,28 @@ package ru.tehalexpetrov.fishhandbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.id_fish -> Toast.makeText(this, "Виды рыб", Toast.LENGTH_SHORT).show()
+            R.id.id_na -> Toast.makeText(this, "Виды наживки", Toast.LENGTH_SHORT).show()
+            R.id.id_sna -> Toast.makeText(this, "Виды снастей", Toast.LENGTH_SHORT).show()
+            R.id.id_history -> Toast.makeText(this, "Истории рыбаков", Toast.LENGTH_SHORT).show()
+        }
+
+        return true
     }
 }
